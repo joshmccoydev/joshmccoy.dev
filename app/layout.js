b477e7d097e0b94b5225a70cdf6e0593f3bfb1e3
josh mccoy <joshmccoydev@icloud.com>
2023-07-21 23:25:19 -0400

    added img switcher, headline text, and styled gradient nav/footer

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import './globals.css'
+import Image from 'next/image'
 import {AiFillGithub, AiFillTwitterCircle} from 'react-icons/ai'
 import {Button} from "@/components/ui/button"
+import logo2 from '/public/logo2.png'
 
 export const metadata = {
   title: 'Josh McCoy - Full-stack Web Developer',
@@ -10,19 +12,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="min-h-full">
-      <body className="container mx-auto">
-        <div className="container fixed top-10 w-full">
-          <nav className="w-3/4 mx-auto bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 justify-between rounded-full flex shadow-lg p-4">
-            <h1 className="text-2xl">jm</h1>
-            <Button className="bg-transparent shadow-lg outline-dashed rounded-3xl text-black">
-              Click me!
+      <body className=" bg-gradient-to-b from-slate-200 to-slate-100 dark:bg-slate-900 dark:text-slate-300">
+        <div className="fixed top-10 w-full z-50">
+          <nav className=" mx-auto dark:bg-none dark:bg-slate-800 w-11/12 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 justify-between rounded-full flex shadow-lg p-4">
+          <span className="flex">
+            <Image src={logo2} className="mx-auto rounded-full"
+              alt="My Logo"
+              width="50"
+              height="50" />
+            <h1 className="text-2xl my-auto">JM</h1>
+            </span>
+            <Button className="my-auto bg-transparent shadow-lg rounded-3xl bg-none bg-slate-200  text-black dark:bg-gradient-to-r dark:from-indigo-500 dark:via-purple-500 dark:to-pink-500">
+              Contact Me
             </Button>
           </nav>
         </div>
         {children}
-        <div className="container fixed bottom-10 w-full">
-          <footer className="w-3/4 mx-auto">
-            <ul className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 justify-center rounded-full flex shadow-lg py-4 text-2xl">
+        <div className="fixed bottom-10 w-full">
+          <footer className="w-full">
+            <ul className="mx-auto w-11/12 dark:bg-none bg-gradient-to-r  from-indigo-500 via-purple-500 to-pink-500 justify-center rounded-full flex shadow-lg py-4 text-2xl">
               <li><AiFillGithub /></li>
               <li><AiFillTwitterCircle /></li>
             </ul>
--- app/page.js
+++ app/page.js
@@ -1,39 +1,50 @@
 import Image from 'next/image'
-import profile from './profile.png'
+import profile from '/public/profile.png'
+import logo from '/public/logo.png'
+import logo2 from '/public/logo2.png'
 
 export default function Home() {
   return (
-    <main className="text-center my-80">
-      <section id="">
-        <h2 className="text-6xl py-2">
-          Joshua McCoy
-        </h2>
-        <h3 className="text-4xl py-2">
-          Full-stack Web Developer
+    <main className="container text-center my-48">
+      <section id="about" className="pb-96">
+        <h3 className="text-5xl z-10 text-left -rotate-[6.75deg]">👋Hello, World!</h3>
+        <div className="flex justify-center pt-2">
+            <h2 className="text-5xl my-auto">I'm&nbsp;</h2>
+            <h2 className="text-6xl my-auto pb-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text dark:bg-gradient-to-r dark:from-indigo-500 dark:via-purple-500 dark:to-pink-500">
+            Josh McCoy
+            </h2>
+        </div>
+        <h3 className="text-4xl dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-indigo-500 dark:via-purple-500 dark:to-pink-500">
+          A prefessional chef by day &< br /> a self-taught, full-stack web developer by night!
         </h3>
-        <p className="text-lg py-2">
+        <p className="text-lg my-2">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum deserunt rem nostrum praesentium ullam molestiae eos tenetur cupiditate amet veniam dignissimos eveniet, recusandae sunt minima quisquam fugit quis. Perferendis, eveniet.
         </p>        
-        <p className="text-lg">
+        <p className="text-lg my-2 mb-4">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum deserunt rem nostrum praesentium ullam molestiae eos tenetur cupiditate amet veniam dignissimos eveniet, recusandae sunt minima quisquam fugit quis. Perferendis, eveniet.
         </p>
 
-        <Image className="mx-auto rounded-full py-20"
-          src={profile}
-          width={400}
-          height={400}
-          alt="Picture of the author"
-        />
+        <div className="cross-fade w-full items-center">
+          <Image src={profile} className="mx-auto rounded-full"
+          alt="My Memoji"
+          width="500"
+          height="500" />
+
+          <Image src={logo} className="mx-auto rounded-full"
+          alt="My Logo"
+          width="500"
+          height="500" />
+        </div>
       </section>
 
-      <section id="skills" className="mt-20">
-        <h3 className="text-2xl text-left">Skills</h3>
+      <section id="skills" className="mt-40">
+        <h3 className="text-2xl text-left">Services</h3>
           <div className="shadow-lg rounded-lg">
             <h4>HTML 5 & CSS3</h4>
             <p className="text-lg p-2">
             </p>
         </div>
-      </section>
+      </section> 
 
       <section id="skills" className="mt-20">
         <h3 className="text-2xl text-left">Tools</h3>
@@ -64,5 +75,6 @@ export default function Home() {
         </div>
       </section>
     </main>
+
   )
 }
